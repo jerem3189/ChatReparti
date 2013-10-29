68a74814145a271c56b18b316fea068943e37f83
Arnaud-Loisy <loisy.arnaud@gmail.com>
2013-10-29 23:57:50 +0100

    room done ?

--- Version 1/Server/src/Room.cpp
+++ Version 1/Server/src/Room.cpp
@@ -5,31 +5,42 @@
 *
 *
 */
-
+using namespace std;
 #include "Room.hpp"
 #include "Client.hpp"
 
-int Room::addClient(Client client) {
+ string name;
+ vector<Client> clients;
 
+int Room::addClient(Client client) {
+    clients.insert(clients.end(),client);
+     
 }
 
 int Room::delClient(Client client) {
+    std::vector<int>::iterator it;
+
+  // iterator pour trouver l'element correspondant au client
+  it = find (clients.begin(), clients.end(), client);
+    clients.erase(it);
 
 }
 
 string Room::getName() {
+    return this->name;
 
 }
 
-void Room::setName(string name) {
-
+void Room::setName(string newName) {
+    this->name=newName;
 }
 
 vector<Client> Room::getClients() {
-
+    return this->clients;
+    
 }
 
 void Room::setClients(vector<Client> clients) {
-
+    this->clients = clients;
 }
 
