279d2257fc1248efce53a8ef3b93c216a3e4c906
root <root@Linux-serge.(none)>
2013-10-30 21:35:22 +0100

    Premier commit (essai)

--- Version 1/Server/src/Socket.cpp
+++ Version 1/Server/src/Socket.cpp
@@ -4,16 +4,33 @@
  */
 
 #include "Socket.hpp"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
 
 int Socket::listen() {
 
 }
 
-int Socket::connect() {
-
+int Socket::connect(SOCKET *csock, SOCKET *sock)
+ { 
+	
 }
 
-int Socket::create() {
-
+int Socket::create(unsigned short = 0) {
+	
+	 SOCKADDR_IN sin; 
+		 
+		if ( (*sock = socket (AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
+		   {
+			 return(m_create = false);
+			  return 1; 
+		   }
 }
 
