15e9d1511b39a342a5912da6dc2b4904f87305f2
root <root@Linux-serge.(none)>
2013-10-30 21:25:41 +0100

    Premier commit (essai)

--- Version 1/Server/src/Socket.hpp
+++ Version 1/Server/src/Socket.hpp
@@ -8,16 +8,41 @@ using namespace std;
 #define _SOCKET_H
 
 #include <string>
-
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <ioctl.h>
+#include <netdb.h>
+#include <netinet/tcp.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
 
 class Socket {
 public:
+
+	Socket();
+	Socket(unsigned short port);
+	~Socket();
+	
+	int connect(unsigned short = 0, bool = false, int = 0);
+	int create(unsigned short = 0, bool = false, int = 0);
 	int listen();
-	int connect();
-	int create();
+	
+	sockaddr_in	get_read_addr();
+	
+	void	terminate();
+	
+	
 private:
 	string ipAddress;
 	int port;
+	bool		m_connect;
+	SOCKET		m_sock;
+	sockaddr_in	m_readaddr;
+	sockaddr_in	m_writeaddr;
+	
 };
 
 #endif  //_SOCKET_H
