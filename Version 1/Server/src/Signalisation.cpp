73b68058cfdc2fd73525d0f11c9c57582c5da03a
andres81500 <serge_lpv@msn.com>
2013-10-31 22:23:41 +0100

    ajout timer

--- Version 1/Server/src/Signalisation.cpp
+++ Version 1/Server/src/Signalisation.cpp
@@ -4,13 +4,37 @@
  */
 
 #include "Signalisation.hpp"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int Signalisation::sendKeepAlive() {
 
 }
 
-void Signalisation::run() {
+void sleep(int nbr_seconds)
+{
+	clock_t goal;
+	goal = (nbr_seconds * CLOCKS_PER_SEC) + clock();
+	while(goal > clock())
+	{
+		;
+	}
+}
 
+int Signalisation::runkeep() {
+
+
+   while(1)
+ {
+    //printf("Il vous reste %ld secondes.\n", i);
+    // ENVOI KEEP ALIVE
+    sleep(1);
+
+
+
+ }
+return 0;
 }
 
 int Signalisation::time() {
--- Version 1/Server/src/Signalisation.hpp
+++ Version 1/Server/src/Signalisation.hpp
@@ -11,7 +11,7 @@
 class Signalisation : public Thread {
 public:
 	int sendKeepAlive();
-	void run();
+	int runkeep();
 private:
 	int time();
 };
