70d87d84c05b9049f3feaef62737f569dedb1aad
Arnaud-Loisy <darkey@Zenbook-UX32VD>
2013-11-01 14:21:16 +0100

    switch serveur cpp

--- Version 1/Server/src/ServeurChat.cpp
+++ Version 1/Server/src/ServeurChat.cpp
@@ -15,6 +15,7 @@ using namespace std;
 #include "Socket.hpp"
 #include "NetworkUDP.hpp"
 #include "RFC1664.hpp"
+#include "MessagesTypesRFC1664.hpp"
 
 
 
@@ -22,7 +23,6 @@ using namespace std;
 #define DEFAULT_PORT "1337"
 
 char message[4096];
-
 
 int main(int argc, char** argv) {
     Socket listenSocket;
@@ -32,27 +32,55 @@ int main(int argc, char** argv) {
     cout << listenSocket.create("0.0.0.0", DEFAULT_PORT);
     cout << listenSocket.binding();
 
-			while (1)
-			{
-                cout << "Main() -> Attente d'un nouveau message" << endl;
-                memset(message, 0, sizeof message);//vide le message
+    while (1) {
+        cout << "Main() -> Attente d'un nouveau message" << endl;
+        memset(message, 0, sizeof message); //vide le message
 
-                udp.receiveDatagrams(listenSocket.getSocket(), message, sizeof message, listenSocket.getSockaddr());
-				
-				cout << "Main() -> Message reçu : " << message << endl;
+        udp.receiveDatagrams(listenSocket.getSocket(), message, sizeof message, listenSocket.getSockaddr());
 
-				/* TRAITEMENT DU MESSAGE
-				if (sendto(listenSocket, message, sizeof message,0,(SOCKADDR *) &clientAddress,clientAddressSize) == -1)
-				{
-					perror("send failed :");
-					close(listenSocket);
-					exit(0);
-				}*/
-				
-				cout << "Main() -> Traitement du message" << endl;
-				cout << "Main() -> Traitement du terminé" << endl;
-			}
+        cout << "Main() -> Message reçu : " << message << endl;
+        string testString(message);
+        RFC1664 rfc;
+                
+        switch (rfc.type(message)) {
+            case MSG_CON:
+                
+                break;
 
-			return 1337;		
+            case MSG_DECO:
+
+                break;
+
+            case MSG_COM:
+                break;
+            case MSG_LIVE:
+
+                break;
+            case MSG_ROOM_JOIN:
+                break;
+            case MSG_ROOM_QUIT:
+                break;
+            case MSG_BOOK_LIST_RQST:
+                break;
+            case MSG_BOOK_LIST_RESP:
+                break;
+            case MSG_RCON:
+                break;
+
+        }
+
+        /* TRAITEMENT DU MESSAGE
+        if (sendto(listenSocket, message, sizeof message,0,(SOCKADDR *) &clientAddress,clientAddressSize) == -1)
+        {
+                perror("send failed :");
+                close(listenSocket);
+                exit(0);
+        }*/
+
+        cout << "Main() -> Traitement du message" << endl;
+        cout << "Main() -> Traitement du terminé" << endl;
+    }
+
+    return 1337;
 }
 
