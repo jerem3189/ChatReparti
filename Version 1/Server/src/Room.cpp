3e8f1c28d22c8e6d5c5ac7660274a61f35a675ef
andres81500 <serge_lpv@msn.com>
2013-11-22 19:09:14 +0100

    quelques commentaires. à finir

--- Version 1/Server/src/Room.cpp
+++ Version 1/Server/src/Room.cpp
@@ -1,16 +1,15 @@
-/*
-* prout file
-*
-*
-*
-*
-*/
+/** \file Room.cpp
+ * 
+ */
 using namespace std;
 #include "Room.hpp"
 #include "Client.hpp"
 #include <vector>
 
-
+/**
+ * Fonction d'ajout de client dans l'annuaire
+ * @param *client informations sur le client
+ */
 int Room::addClient(Client *client) {
     cout << "Room::addClient() -> Ajout du client " << client->getName() << " dans la room " << this->getName() << "." << endl;
     this->clients.push_back(client);
@@ -18,6 +17,10 @@ int Room::addClient(Client *client) {
     return 1;
 }
 
+/**
+ * Fonction de suppression de client dans l'annuaire
+ * @param *client informations sur le client
+ */
 int Room::delClient(Client *client) {
     vector<Client*>::iterator it = this->findClient(client->getName());
     if (it == this->clients.end())
@@ -32,23 +35,33 @@ int Room::delClient(Client *client) {
 
     return 0;
 }
-
+/**
+ * getter du nom d'un client
+ * @return name le nom du client
+ */
 string Room::getName() {
     return this->name;
 }
-
+/**
+ * setter du nom d'un client 
+ * @param newName le nouveau nom attribué
+ */
 void Room::setName(string name) {
     this->name=name;
 }
-
+/**
+ */
 vector<Client*> Room::getClients() {
     return this->clients;
 }
-
+/**
+ */
 void Room::setClients(vector<Client*> clients) {
     this->clients = clients;
 }
 
+/**
+ */
 vector<Client*>::iterator Room::findClient(string name) {
     vector<Client*>::iterator it;
     for(it = this->clients.begin(); it != this->clients.end(); ++it)
